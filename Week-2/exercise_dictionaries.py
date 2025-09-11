f53b57f03d15378eac18c2ea60f154d4fd743cb3
José Iván Amaya <joseivan.amaya10@gmail.com>
2025-09-10 22:55:04 -0500

    Añadí el archivo de ejecicio con diccionarios

---
+++ Week-2/exercise_dictionaries.py
@@ -0,0 +1,15 @@
+fhand = open("C:/Users/57300/OneDrive/Documentos/GitHub/Learning-Python/Week-2/mbox-short.txt")
+lista_dias = list()
+for line in fhand:
+    line = line.strip()
+    if line.startswith("From"):
+        line = line.split()
+        if len(line) < 3:
+            continue
+        days = line[2]
+        lista_dias.append(days)
+days = lista_dias
+counts = dict()
+for day in days:
+    counts[day] = counts.get(day, 0) + 1
+print(counts)
